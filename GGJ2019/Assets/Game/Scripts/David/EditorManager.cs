a2096b79aaf24536e5f02cbe50d867e865616a6f
David Jansen <davidjejans@hotmail.nl>
2019-01-26 11:28:45 +0100

    EditorMode&TrapSelect

--- GGJ2019/Assets/Game/Scripts/David/EditorManager.cs
+++ GGJ2019/Assets/Game/Scripts/David/EditorManager.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EditorManager : MonoBehaviour
 {
+	public int CurrentTrap;
 	[SerializeField] private List<GameObject> m_Traps;
-	[SerializeField] public int CurrentTrap;
+	[SerializeField] private TextMeshProUGUI m_EditorModeText;
+	private bool m_InEditorMode;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,43 @@ public class EditorManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+		if (Input.GetKeyDown(KeyCode.Q)){
+			m_InEditorMode = !m_InEditorMode;
+		}
+
+		if (m_InEditorMode)
+		{
+			m_EditorModeText.text = "In EditorMode";
+		}
+		else if (!m_InEditorMode)
+		{
+			m_EditorModeText.text = "";
+		}
     }
+
+	private void TrapSelect()
+	{
+		if (Input.GetKeyDown(KeyCode.F1))
+		{
+			CurrentTrap = 0;
+		}
+		else if (Input.GetKeyDown(KeyCode.F2))
+		{
+			CurrentTrap = 1;
+		}
+		else if (Input.GetKeyDown(KeyCode.F3))
+		{
+			CurrentTrap = 2;
+		}
+		else if (Input.GetKeyDown(KeyCode.F4))
+		{
+			CurrentTrap = 3;
+		}
+		else if (Input.GetKeyDown(KeyCode.F5))
+		{
+			CurrentTrap = 4;
+		}
+	}
+
+
 }
---
+++ GGJ2019/Assets/Game/Scripts/David/PlayerController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] private float m_ForwardsPlayerSpeed;
+	[SerializeField] private float m_BackwardsPlayerSpeed;
+	[SerializeField] private float m_SidewaysPlayerSpeed;
+	[SerializeField] private float m_JumpPlayerSpeed;
+
+	private bool m_ForwardPressed;
+	private bool m_BackPressed;
+	private bool m_RightPressed;
+	private bool m_LeftPressed;
+	private bool m_JumpPressed;
+
+	private float m_GroundDistance;
+	private bool m_IsGrounded;
+	private Rigidbody m_Rigidbody;
+
+	private void Start()
+	{
+		m_Rigidbody = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+
+		// moving depending on the keyinput
+		if (Input.GetKey(KeyCode.W))
+		{
+			m_ForwardPressed = true;
+		}
+		else
+		{
+			m_ForwardPressed = false;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			m_BackPressed = true;
+		}
+		else
+		{
+			m_BackPressed = false;
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			m_LeftPressed = true;
+		}
+		else
+		{
+			m_LeftPressed = false;
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			m_RightPressed = true;
+		}
+		else
+		{
+			m_RightPressed = false;
+		}
+		if (Input.GetKey(KeyCode.Space))
+		{
+			m_JumpPressed = true;
+		}
+		else
+		{
+			m_JumpPressed = false;
+		}
+
+		if (Physics.Raycast(transform.position, Vector3.down, 1.1f))
+		{
+			m_IsGrounded = true;
+		}
+		else
+		{
+			m_IsGrounded = false;
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		// moving depending on the keyinput
+		if (m_ForwardPressed)
+		{
+			transform.Translate(Vector3.forward * m_ForwardsPlayerSpeed * Time.deltaTime);
+
+		}
+		if (m_BackPressed)
+		{
+			transform.Translate(Vector3.forward * -m_BackwardsPlayerSpeed * Time.deltaTime);
+
+		}
+		if (m_LeftPressed)
+		{
+			transform.Translate(Vector3.left * m_SidewaysPlayerSpeed * Time.deltaTime);
+		}
+		if (m_RightPressed)
+		{
+			transform.Translate(Vector3.right * m_SidewaysPlayerSpeed * Time.deltaTime);
+		}
+		if (m_JumpPressed && m_IsGrounded == true)
+		{
+			m_Rigidbody.AddForce(Vector3.up * m_JumpPlayerSpeed, ForceMode.Impulse);
+		}
+	}
+}
