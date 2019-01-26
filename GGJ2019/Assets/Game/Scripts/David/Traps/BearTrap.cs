355b8083d796b2dd75f3af993a036c9084e0b4e8
David Jansen <davidjejans@hotmail.nl>
2019-01-26 14:53:31 +0100

    beartrap temp push

--- GGJ2019/Assets/Game/Scripts/David/Traps/BearTrap.cs
+++ GGJ2019/Assets/Game/Scripts/David/Traps/BearTrap.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class BearTrap : MonoBehaviour
 {
 	[SerializeField] private float m_Damage;
+	private bool m_CurrentlyActive;
 
 	private void OnTriggerEnter(Collider other)
 	{
-		if (other.CompareTag("Enemy"))
+		if (other.CompareTag("Enemy") )
 		{
-			other.GetComponent<AverageEnemy>().TakeDamage(m_Damage);
+			AverageEnemy enemyHit = other.GetComponent<AverageEnemy>();
+			enemyHit.TakeDamage(m_Damage);
+			//other.GetComponent<AverageEnemy>()
 		}
 	}
 }
